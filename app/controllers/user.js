3732688b2bfc9ab971b80e939d443bb813bd5e66
Mario Andres Portela Gaviria <mario.portela@wolox.co>
2019-06-21 07:09:26 -0500

    users route created

---
+++ app/controllers/user.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+const errors = require('../errors');
+const { User } = require('../models');
+
+exports.signUp = (req, res, next) => {
+  res.send(req.body);
+};
+/* eslint-disable no-unused-vars */
--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,8 @@
 // const controller = require('./controllers/controller');
 const { healthCheck } = require('./controllers/healthCheck');
+const users = require('./controllers/user');
+
 exports.init = app => {
   app.get('/health', healthCheck);
-  // app.get('/endpoint/get/path', [], controller.methodGET);
-  // app.put('/endpoint/put/path', [], controller.methodPUT);
-  // app.post('/endpoint/post/path', [], controller.methodPOST);
+  app.post('/users', users.signUp);
 };
